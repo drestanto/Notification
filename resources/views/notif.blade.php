65e38e5251e20b7d9146daf634b39f74caff66ca
Dyas <dyas@live.com>
2017-06-09 10:29:43 +0700

    edit notif view

--- resources/views/notif.blade.php
+++ resources/views/notif.blade.php
@@ -4,7 +4,18 @@
 </head>
 <body>
 	<h3> Give notif to other user </h3>
-	<form action="/submitNotif" method="post">
-	</form>
+	@if (Auth::check())
+		<form action="/submitNotif" method="post">
+			Choose User : <select name="user">
+				@foreach ($users as $user)
+					@if ($user->id != Auth::id())
+						<option value="{{$user->id}}">{{ $user->name }}</option>
+					@endif
+				@endforeach
+			</select><br>
+			<input type="submit" value="Notify!">
+		</form>
+	@else You need to Login before using this feature.
+	@endif
 </body>
 </html>
